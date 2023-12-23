4bc8f243e729347eec2d986fa05fd68242cd47c4
Shohag Hossain <santo.shohag.22@gmail.com>
2023-12-23 18:34:18 +0600

    Home 1.0.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Home Page</h1>
-      <p>Welcome to the home page!</p>
+      <p>Welcome to the home page! 1.0.0</p>
     </div>
   );
 };
